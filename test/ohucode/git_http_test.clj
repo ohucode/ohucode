519e406251c3d951aaf12242183c24e2bacc3ed4
Daehyun Kim <hatemogi@gmail.com>
2015-10-08 10:18:15 +0900

    smart http 핸들러 테스트코드 정리

---
+++ test/ohucode/git_http_test.clj
@@ -0,0 +1,51 @@
+(ns ohucode.git-http-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [ring.mock.request :as mock]
+            [ohucode.handler :refer [app]]
+            [ohucode.git-http :refer :all]))
+
+(deftest git-http-route
+  (testing "/info/refs should response with a proper content-type"
+    (let [res (app (mock/request :get "/u/p/info/refs?service=git-upload-pack"))]
+      (is (= (get-in res [:headers "Content-Type"])
+             "application/x-git-upload-pack-advertisement"))
+      (is (= (:status res) 200)))
+    (let [res (app (mock/request :get "/u/p/info/refs?service=git-receive-pack"))]
+      (is (= (get-in res [:headers "Content-Type"])
+             "application/x-git-receive-pack-advertisement"))))
+  
+  (testing "POST /git-upload-pack"
+    (let [file (io/as-file "fixture/upload-pack-req.body")
+          req (merge
+               (mock/request :post "/u/p/git-upload-pack")
+               {:headers {"Content-Length" (.length file)
+                          "Content-Type" "application/x-git-upload-pack-request"
+                          "Accept" "application/x-git-upload-pack-result"
+                          "Content-Encoding" "gzip"
+                          "Accept-Encoding" "gzip"}
+                :body (io/input-stream file)})
+          res (app req)]
+      (is (= (:status res) 200))
+      (are [key value] (= (get-in res [:headers key]) value)
+        "Content-Type" "application/x-git-upload-pack-result"
+        "Content-Encoding" "gzip"
+        "Cache-Control" "no-cache, max-age=0, must-revalidate")))
+  
+  (testing "POST /git-receive-pack"
+    (let [create-branch (io/as-file "fixture/receive-pack-create-branch.body")
+          delete-branch (io/as-file "fixture/receive-pack-delete-branch.body")]
+      (doseq [fixture-file [create-branch delete-branch]]
+        (let [req (merge
+                   (mock/request :post "/u/p/git-receive-pack")
+                   {:headers {"Content-Length" (.length fixture-file)
+                              "Content-Type" "application/x-git-receive-pack-request"
+                              "Accept" "application/x-git-receive-pack-result"
+                              "Accept-Encoding" "gzip"}
+                    :body (io/input-stream fixture-file)})
+              res (app req)]
+          (is (= (:status res) 200))
+          (are [key value] (= (get-in res [:headers key]) value)
+            "Content-Type" "application/x-git-receive-pack-result"
+            "Content-Encoding" "gzip"
+            "Cache-Control" "no-cache, max-age=0, must-revalidate"))))))
