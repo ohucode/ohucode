0dfb65c2ea44fa2496c493df7fea2b6b8bc34b8a
Daehyun Kim <hatemogi@gmail.com>
2015-10-08 14:56:40 +0900

    개발모드에서 핸들러도 리로드 적용되게끔 수정

--- src/ohucode/handler.clj
+++ src/ohucode/handler.clj
@@ -3,57 +3,65 @@
             [compojure.route :as route]
             [ring.util.response :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]
-            [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.lint :refer [wrap-lint]]
             [ring.middleware.session.cookie :refer [cookie-store]]
             [ring.logger.timbre :refer [wrap-with-logger]]
+            [taoensso.timbre :as timbre]
             [aleph.http :as http]
             [ohucode.view :as view]
             [ohucode.git :as git]
             [ohucode.git-http :refer [smart-http-routes]])
   (:import [java.util Locale]))
 
-(defn index [req] "한글 인덱스 뷰! 별도 함수 리로드 확인. 이건 알레프도 되요.")
-
-(println "핸들러 로드")
-
-(defn- not-implemented-yet [req]
+(defn- not-implemented [req]
   (throw (UnsupportedOperationException.)))
+
+(def user-routes
+  (context "/:user" [user]
+    (GET "/" [] not-implemented)
+    (GET "/settings" [] not-implemented)
+    (GET "/profile" [] not-implemented)))
 
 (def project-routes
   (context "/:user/:project" [user project]
     (GET "/" [] (str user "/" project))
-    (GET "/commits" [] not-implemented-yet)
-    (GET "/settings" [] not-implemented-yet)
-    (GET "/tree/:ref/:path" [path] not-implemented-yet)
-    (GET "/blob/:ref/:path" [path] not-implemented-yet)
-    (GET "/tags" [] not-implemented-yet)
-    (GET "/issues" [] not-implemented-yet)))
+    (GET "/commits" [] not-implemented)
+    (GET "/commits/:ref" [ref] not-implemented)
+    (GET "/commit/:commit-id" not-implemented-yet)
+    (GET "/settings" [] not-implemented)
+    (GET "/tree/:ref/:path" [ref path] not-implemented)
+    (GET "/blob/:ref/:path" [ref path] not-implemented)
+    (GET "/tags" [] not-implemented)
+    (GET "/branches" [] not-implemented)
+    (GET "/issues" [] not-implemented)))
 
 (def web-routes
   (routes
-   (GET "/" [] index)
+   (GET "/" [] "리로드?")
    (POST "/" [] "post test")
+   user-routes
    project-routes))
 
-(def app-routes
+(def app
   (routes
    (wrap-defaults web-routes
-                  (update site-defaults :session
-                          merge {:store (cookie-store)}))
+                  (update site-defaults
+                          :session merge
+                          {:store (cookie-store "ohucode passkey")}))
    (wrap-defaults smart-http-routes api-defaults)
    (route/resources "/")
    (route/not-found "Page not found")))
 
-(def app app-routes)
-
 (def app-dev
   (-> app
       (wrap-reload)
+      (wrap-lint)
       (wrap-with-logger)))
 
-;; start-server로 시작한 내용은 리로드되지 않음. 왜?
 (defn start []
-  (Locale/setDefault Locale/US)
-  (http/start-server app-dev {:port 10000}))
+  (let [port 10000]
+    (Locale/setDefault Locale/US)
+    (timbre/info (str "Starting http-server on " port))
+    (let [handler-for-reload #(app-dev %)]
+      (http/start-server handler-for-reload {:port port}))))
