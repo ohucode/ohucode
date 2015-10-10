2f916efe199e7fd8aefd6d663f48d1d4f997cc39
Daehyun Kim <hatemogi@gmail.com>
2015-10-10 21:14:44 +0900

    템플릿 스니핏 임베딩 처리

--- src/ohucode/view.clj
+++ src/ohucode/view.clj
@@ -1,7 +1,7 @@
 (ns ohucode.view
   (:require [net.cgrand.enlive-html :as h]
             [net.cgrand.reload :as reload]
-            [ring.middleware.anti-forgery :as af]))
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
 
 (h/defsnippet navigation "templates/navigation.html"
   [:nav]
@@ -15,25 +15,25 @@
   [{:keys [title main]}]
   [:title] (h/content title)
   [:nav] (h/substitute (navigation))
-  [:main] (h/substitute main)
+  [:main] (h/substitute (main))
   [:footer] (h/substitute (footer)))
-
-(h/defsnippet intro-guest-snippet "templates/intro_guest.html"
-  [:main]
-  []
-  [:#sign-up-form] (h/append
-                    (h/html
-                     [:input {:type "hidden" :value af/*anti-forgery-token*}])) ;
-  )
 
 (h/defsnippet dashboard "templates/dashboard.html"
   [:main]
   []
   )
 
+(defn anti-forgery-field []
+  (h/html [:input {:type "hidden" :value *anti-forgery-token*}]))
+
 (defn intro-guest []
-  (layout {:title "오후코드"
-           :main (intro-guest-snippet)}))
+  (layout
+   {:title "오후코드 첫화면"
+    :main (h/snippet
+           "templates/intro_guest.html"
+           [:main] []
+           [:#sign-up-form]
+           (h/append (anti-forgery-field)))}))
 
 ;; TODO: make reloading affect only in dev mode
 (reload/auto-reload *ns*)
