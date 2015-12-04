10761f9e46d80e1709d8a3cfc8d6fa5de9618feb
Daehyun Kim <hatemogi@gmail.com>
2015-12-04 15:24:41 +0900

    random-digest 테스트 추가

--- test/ohucode/password_test.clj
+++ test/ohucode/password_test.clj
@@ -9,12 +9,18 @@
       "U0WSjbIsUg6DvHVmGi6oRXr/KSA=" (pbkdf2 "password" "0000salt" 100000 160)
       "tOE5IC6zqHxfbexkOfSIQtyqknA=" (pbkdf2 "other"    "salt1234" 100000 160)
       "/11bL9NO6j7Ho7ZLw929RfZve0Y=" (pbkdf2 "other"    "0000salt" 100000 160)))
+
   (testing "digest and validate"
     (dotimes [i 5]
       (let [password (str "password-" (random-int))
             salt (str "salt-" (random-int))
             raw (password-digest password salt)]
         (is (valid-password-digest? raw password salt)))))
+
   (testing "random-6-digits should have no duplicates in a limited condition"
     (let [xs (repeatedly 100 random-6-digits)]
+      (is (= (count xs) (count (set xs))))))
+
+  (testing "random-digest should have no duplicates in a limited condition"
+    (let [xs (repeatedly 10000 random-digest)]
       (is (= (count xs) (count (set xs)))))))
