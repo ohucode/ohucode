5f475f6513913880bacf7119c38da3efdcce8a2b
Daehyun Kim <hatemogi@gmail.com>
2015-12-10 16:30:56 +0900

    가입신청폼 개발

--- src/ohucode/view_signup.clj
+++ src/ohucode/view_signup.clj
@@ -19,52 +19,61 @@
    [:div.form-group.pull-right
     [:button.btn.btn-primary {:type "submit" :disabled "{{!valid_form}}"} "다음 >"]]])
 
+(def ^:private signup-step-texts
+  ["아이디/이메일 입력"
+   "확인 코드 입력"
+   "기본 프로필"
+   "이용약관 동의"])
+
 (defn- signup-progress [active-step]
   [:ul.nav.nav-pills.nav-stacked
-   (for [[step text] (map vector
-                          (iterate inc 1)
-                          ["아이디/이메일 입력"
-                           "이메일 주소 확인"
-                           "기본 프로필"
-                           "이용약관 동의"])]
+   (for [[step text] (map vector (iterate inc 1) signup-step-texts)]
      [:li.disabled {:class (if (= step active-step) "active" "disabled")}
       [:a {:href "#"} (str step ". " text)]])])
 
+(defn- signup-layout [active-step _ & body]
+  (let [title (brand-name+ "가입 > " active-step "단계")]
+    (layout {:title title}
+            [:div.container
+             [:div.row
+              [:div.page-header [:h1 title]]]
+             [:div.row
+              [:div.col-sm-3 (signup-progress active-step)]
+              [:div.col-sm-9
+               [:div.row
+                [:h2 (signup-step-texts (dec active-step))]
+                body]]]])))
+
 (defn signup-step1 [_]
   "가입 1단계: 아이디와 이메일 접수"
-  (layout {:title (brand-name+ "가입")}
-          [:div.container
-           [:div.row
-            [:div.page-header [:h1 (brand-name+ "가입 신청")]]]
-           [:div.row
-            [:div.col-sm-3 (signup-progress 1)]
-            [:div.col-sm-9
-             
-             [:h2 "아이디/이메일 입력"]
-             [:div.row
-              [:div.col-sm-6
-               (signup-form _)]]]]]))
+  (signup-layout 1 _
+                 [:div.row
+                  [:div.col-sm-7
+                   (signup-form _)]]))
 
 (defn signup-step2 [_]
-  "가입 2단계: 이메일 확인"
-  (layout {:title (brand-name+ "가입 > 2단계")}
-          [:div.container
-           [:div.row
-            [:div.col-sm-3 (signup-progress 2)]
-            [:div.col-sm-9
-             [:div.page-header [:h1 (brand-name+ "가입 신청")]]
-             [:div.page-header [:h2 "이메일 주소 확인"]]
-             [:p "입력하신 이메일 주소로 확인 메일을 보내드렸습니다. 메일을 확인하셔서 "
-              [:strong "가입확인코드"]
-              "를 아래에 입력해주세요."]
-             [:div.row
-              [:div.col-sm-6.col-md-4
-               [:form#confirm-form
-                [:div.input-group.input-group-lg
-                 [:input.form-control.input-lg {:type "text" :placeholder "######" :autofocus true}]
-                 [:span.input-group-btn.input-group-lg
-                  [:button.btn.btn-primary "확인"]]]]]]
-             
-             ]]]))
+  "가입 2단계: 메일 확인코드 입력"
+  (signup-layout 2 _
+                 [:div.panel.panel-default
+                  [:div.panel-body
+                   [:form#confirm-form.form-horizontal
+                    [:div.form-group
+                     [:label.control-label.col-xs-3.col-sm-2 "이메일"]
+                     [:div.form-control-static.col-xs-6.col-sm-10 "hatemogi@gmail.com"]]
+                    [:div.form-group
+                     [:label.control-label.col-xs-3.col-sm-2 {:for "confirm-code"} "확인코드"]
+                     [:div.col-xs-6.col-sm-3
+                      [:div.input-group
+                       [:input#confirm-code.form-control {:v-model "code" :type "text" :placeholder "######" :autofocus true}]
+                       [:span.input-group-btn [:button.btn.btn-primary "확인"]]]]]]]]
+                 [:p "위 이메일 주소로 확인 코드를 보냈습니다. 보내드린 메일에 적혀있는 6자리 "
+                  [:strong "확인코드"]
+                  "를 입력해주세요."]
+                 [:div.row
+                  [:div.col-sm-3
+                   [:form#confirm-form
+                    ]]]
+                 [:p.text-right "다른 이메일 주소로 가입하시겠어요? > "
+                  [:a {:href "#"} "1단계에서 다시 시작"]]))
 
 (println (str *ns* " reloaded"))
