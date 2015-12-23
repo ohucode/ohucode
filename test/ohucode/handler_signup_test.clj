cdc4c3e968e75223c9b658064076f6b285db2cd0
Daehyun Kim <hatemogi@gmail.com>
2015-12-23 16:01:55 +0900

    기타 미들웨어 빼고 테스트

--- test/ohucode/handler_signup_test.clj
+++ test/ohucode/handler_signup_test.clj
@@ -2,16 +2,12 @@
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
             [ring.mock.request :as mock]
-            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
+            [ring.middleware.params :refer [wrap-params]]
             [ohucode.handler :refer [app]]
             [ohucode.handler-signup :refer :all]))
 
 (deftest test-signup
-  (let [req (comp app
-                  ;; TODO: 중간 다른 미들웨어에서 세션 값을 바꾸는 것 같다.
-                  (fn [r] (pr r) r)
-                  #(assoc-in % [:session :ring.middleware.anti-forgery/anti-forgery-token] "test")
-                  #(assoc-in % [:headers "x-csrf-token"] "test")
+  (let [req (comp (wrap-params signup-routes)
                   mock/request)]
     (comment testing "show step1"
       (let [res (req :get "/signup")]
@@ -25,5 +21,4 @@
 
     (testing "step1: 확인코드 신청"
       (let [res (req :post "/signup/2" {:userid "test"})]
-        (println res)
         (is (= (:status res) 200))))))
