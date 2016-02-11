f31aa78de00b221c714344fed12252ab6d3ea5a5
Daehyun Kim <hatemogi@gmail.com>
2016-02-11 21:34:38 +0900

    서버 인스턴스 atom 처리

--- src/오후코드/서버.clj
+++ src/오후코드/서버.clj
@@ -6,18 +6,21 @@
             [cider.nrepl :refer [cider-nrepl-handler]])
   (:import [java.util Locale]))
 
+(defonce servers (atom {}))
+
 (defn 시작 []
-  (defonce http-server
-    (let [port 10000]
+  (let [http-port 10000
+        repl-port 7888
+        handler-for-reload #(app-dev %)]
+    (do
       (Locale/setDefault Locale/US)
-      (timbre/info (str "Starting http-server on " port))
-      (let [handler-for-reload #(app-dev %)]
-        (http/start-server handler-for-reload {:port port}))))
-  (defonce repl-server
-    (let [port 7888]
-      (timbre/info (str "Starting nREPL on " port))
-      (start-server :port port :handler cider-nrepl-handler))))
+      (timbre/info (str "Starting http-server on " http-port))
+      (timbre/info (str "Starting nREPL on " repl-port))
+      (let [http-server (http/start-server handler-for-reload {:port http-port})
+            repl-server (start-server :port repl-port :handler cider-nrepl-handler)]
+        (swap! servers assoc :http-server http-server)
+        (swap! servers assoc :repl-server repl-server)))))
 
 (defn 중단 []
-  (.close http-server)
-  (stop-server repl-server))
+  (.close (:http-server @servers))
+  (stop-server (:repl-server @servers)))
