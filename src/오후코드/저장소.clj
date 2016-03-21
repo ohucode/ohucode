5d8db308310e8af6840b14a89520f5fb4edffb8f
Daehyun Kim <hatemogi@gmail.com>
2016-03-21 15:58:23 +0900

    저장소레코드(defrecord) 추가

--- src/오후코드/저장소.clj
+++ src/오후코드/저장소.clj
@@ -10,11 +10,17 @@
            [org.eclipse.jgit.api.errors RefNotFoundException]
            [java.io File FileNotFoundException]))
 
-(defrecord 커서 [아이디 프로젝트명 레프 경로])
+(정의 ^{:dynamic 참
+        :doc "저장소를 읽고 쓸 최상위 디렉토리."}
+  *저장소위치* "저장소")
 
-(defrecord 저장소 [아이디 프로젝트명 리포])
+(레코드 커서 [아이디 프로젝트명 레프 경로])
 
-(정의 ^:dynamic *저장소위치* "저장소")
+(선언 닫기)
+
+(레코드 저장소레코드 [아이디 프로젝트명 리포]
+  java.io.Closeable
+  (close [this] (닫기 this)))
 
 (함수 저장소경로 [아이디 프로젝트명]
   (clojure.string/join "/" [*저장소위치* 아이디 프로젝트명]))
@@ -22,25 +28,29 @@
 (함수- 저장소파일 [아이디 프로젝트명]
   (.. (File. (저장소경로 아이디 프로젝트명)) getAbsoluteFile))
 
-(함수 ^Repository 열기 [아이디 프로젝트명]
+(함수 ^저장소레코드 열기 [아이디 프로젝트명]
   "로컬 파일 저장소를 연다."
-  (.. (FileRepositoryBuilder.)
-      (setGitDir (저장소파일 아이디 프로젝트명))
-      (setMustExist true)
-      build))
+  (가정 [리포 (.. (FileRepositoryBuilder.)
+                  (setGitDir (저장소파일 아이디 프로젝트명))
+                  (setMustExist 참)
+                  build)]
+    (->저장소레코드 아이디 프로젝트명 리포)))
 
-(함수 ^Repository 생성! [아이디 프로젝트명]
+(함수 닫기 [^저장소레코드 저장소]
+  (.close (:리포 저장소)))
+
+(함수 ^저장소레코드 생성! [아이디 프로젝트명]
   "로컬 파일 시스템에 빈 bare 저장소를 새로 만든다."
-  (doto (.. (FileRepositoryBuilder.)
-            setBare
-            (setGitDir (저장소파일 아이디 프로젝트명))
-            build)
-    (.create true)))
+  (가정 [리포 (doto (.. (FileRepositoryBuilder.)
+                        setBare
+                        (setGitDir (저장소파일 아이디 프로젝트명))
+                        build)
+                (.create 참))]
+    (->저장소레코드 아이디 프로젝트명 리포)))
 
 (함수- rm-rf! [경로]
-  (when (.isDirectory 경로)
-    (doseq [파일 (.listFiles 경로)]
-      (rm-rf! 파일)))
+  (만약 (.isDirectory 경로)
+    (run! rm-rf! (.listFiles 경로)))
   (.delete 경로))
 
 (정의 삭제! (합성 rm-rf! 저장소파일))
