0c37ed88ba1f4fbf9b329b04e78208f4e0ebdefc
Daehyun Kim <hatemogi@gmail.com>
2016-03-23 21:26:06 +0900

    권한 검사 미들웨어 적용해서 라우트 정리

--- src/오후코드/핸들러.clj
+++ src/오후코드/핸들러.clj
@@ -15,60 +15,70 @@
             [오후코드.보안 :as 보안]
             [오후코드.뷰 :as 뷰]
             [오후코드.저장소 :as 저장소]
+            [오후코드.권한 :as 권한]
             [오후코드.핸들러-로그인 :refer [자동로그인-미들웨어 로그인-라우트]]
             [오후코드.핸들러-깃 :refer [smart-http-라우트]]
             [오후코드.핸들러-관리 :refer [관리-라우트]]
             [오후코드.핸들러-가입 :refer [가입-라우트]]))
 
-(정의 이용자-라우트
-  (context "/:아이디" [아이디]
-    (GET "/" [] (만약-가정 [이용자 (db/이용자-열람 아이디)]
-                  (str 이용자)))
-    (POST "/" [프로젝트명 설명 공개?]
-      (println 아이디 프로젝트명 설명 공개?)
-      (조건
-       (db/프로젝트-열람 아이디 프로젝트명)
-       {:status 409 :body {:실패 "이미 있는 프로젝트명"}}
+(매크로 미들웨어-라우트 [& 본문]
+  (가정 [미들웨어 (첫째 본문)
+         라우트목록 (잔여 본문)]
+    `(compojure.core/wrap-routes (compojure.core/routes ~@라우트목록)
+                                 ~@미들웨어)))
 
-       참
-       (만약-가정 [이용자 (db/이용자-열람 아이디)]
-         ;; TODO: 권한검사, 프로젝트명 유효성 확인, 중복 검사
-         (db/트랜잭션
-          (db/프로젝트-생성 아이디 프로젝트명 설명 공개?)
-          (가정 [ㅈ (저장소/생성! 아이디 프로젝트명)]
-            {:status 200
-             :body {:저장소 ㅈ}})))))
-    (GET "/settings" [] 뷰/미구현)
-    (GET "/profile" [] 뷰/미구현)))
+(매크로대응 라우트정의 defroutes)
 
-(정의 프로젝트-라우트
-  (context "/:아이디/:프로젝트" [아이디 프로젝트명]
-    (GET "/" [] {:프로젝트 {}
-                 :브랜치목록 []
-                 :커밋목록 []
-                 :트리 []})
-    (GET "/commits" [] 뷰/미구현)
-    (GET "/commits/:ref" [ref] 뷰/미구현)
-    (GET "/commit/:commit-id" [commit-id] 뷰/미구현)
-    (GET "/settings" [] 뷰/미구현)
-    (GET "/tree/:ref/:path" [ref path] 뷰/미구현)
-    (GET "/blob/:ref/:path" [ref path] 뷰/미구현)
-    (GET "/tags" [] 뷰/미구현)
-    (GET "/branches" [] 뷰/미구현)
-    (GET "/issues" [] 뷰/미구현)))
+(라우트정의 터전-라우트
+  (context "/:터전명" [터전명]
+    (미들웨어-라우트 (권한/터전-읽는-미들웨어 터전명)
+                     (GET "/" 요청 (str (get-in 요청 [:오후코드 :터전주인])))))
+  (context "/:터전명" [터전명]
+    (미들웨어-라우트 (권한/터전-쓰는-미들웨어 터전명)
+                     (POST "/" [프로젝트명 설명 공개?]
+                       (조건
+                        (db/프로젝트-열람 터전명 프로젝트명)
+                        {:status 409 :body {:실패 "이미 있는 프로젝트명"}}
 
-(정의 웹-라우트
-  (routes
-   (GET "/" [] 뷰/기본)
-   (GET "/throw" [] (예외발생 (RuntimeException. "스택트레이스 실험")))
-   (GET "/tos" [] 뷰/기본)
-   (GET "/policy" [] 뷰/기본)
-   (GET "/credits" [] 뷰/기본)
-   가입-라우트
-   관리-라우트
-   로그인-라우트
-   이용자-라우트
-   프로젝트-라우트))
+                        참
+                        (만약-가정 [터전 (db/이용자-열람 터전명)]
+                          ;; TODO: 권한검사, 프로젝트명 유효성 확인, 중복 검사
+                          (db/트랜잭션
+                           (db/프로젝트-생성 터전명 프로젝트명 설명 공개?)
+                           (가정 [ㅈ (저장소/생성! 터전명 프로젝트명)]
+                             {:status 200
+                              :body {:저장소 ㅈ}})))))
+                     (GET "/settings" [] 뷰/미구현)
+                     (GET "/profile" [] 뷰/미구현))))
+
+(라우트정의 프로젝트-라우트
+  (context "/:터전명/:플젝명" [터전명 플젝명]
+    (미들웨어-라우트 (권한/플젝-읽는-미들웨어 터전명 플젝명)
+                     (GET "/" [] (println "프로젝트 루트에 걸렸나?") {:프로젝트 {}
+                                                                      :브랜치목록 []
+                                                                      :커밋목록 []
+                                                                      :트리 []})
+                     (GET "/commits" [] 뷰/미구현)
+                     (GET "/commits/:ref" [ref] 뷰/미구현)
+                     (GET "/commit/:commit-id" [commit-id] 뷰/미구현)
+                     (GET "/settings" [] 뷰/미구현)
+                     (GET "/tree/:ref/:path" [ref path] 뷰/미구현)
+                     (GET "/blob/:ref/:path" [ref path] 뷰/미구현)
+                     (GET "/tags" [] 뷰/미구현)
+                     (GET "/branches" [] 뷰/미구현)
+                     (GET "/issues" [] 뷰/미구현))))
+
+(라우트정의 웹-라우트
+  (GET "/" [] 뷰/기본)
+  (GET "/throw" [] (예외발생 (RuntimeException. "스택트레이스 실험")))
+  (GET "/tos" [] 뷰/기본)
+  (GET "/policy" [] 뷰/기본)
+  (GET "/credits" [] 뷰/기본)
+  가입-라우트
+  관리-라우트
+  로그인-라우트
+  터전-라우트
+  프로젝트-라우트)
 
 (함수- 콘텐트타입-미들웨어 [핸들러]
   (fn [요청]
@@ -121,29 +131,28 @@
    :doc "리로드해도 세션을 유지하기 위해 메모리 세션 따로 둔다"}
   세션저장소 (memory-store))
 
-(정의 앱라우트
-  (routes
-   (route/resources "/js"  {:root "public/js"})
-   (route/resources "/css" {:root "public/css"})
-   (route/resources "/md"  {:root "public/md"})
+(라우트정의 앱라우트
+  (route/resources "/js"  {:root "public/js"})
+  (route/resources "/css" {:root "public/css"})
+  (route/resources "/md"  {:root "public/md"})
 
-   (wrap-routes smart-http-라우트
-                wrap-defaults api-defaults)
+  (wrap-routes smart-http-라우트
+               wrap-defaults api-defaults)
 
-   (-> 웹-라우트
-       wrap-with-logger          ; TODO: 로거 위치 고민 필요
-       이용자-바인딩-미들웨어
-       ip-바인딩-미들웨어
-       콘텐트타입-미들웨어
-       자동로그인-미들웨어
-       edn-미들웨어
-       (wrap-defaults (-> site-defaults
-                          ;; static 자원은 앞에서 미리 처리한다
-                          (dissoc :static)
-                          (dissoc :security) ;; TODO: AJAX CSRF 대응
-                          (assoc-in [:session :store] 세션저장소))))
+  (-> 웹-라우트
+      wrap-with-logger          ; TODO: 로거 위치 고민 필요
+      이용자-바인딩-미들웨어
+      ip-바인딩-미들웨어
+      콘텐트타입-미들웨어
+      자동로그인-미들웨어
+      edn-미들웨어
+      (wrap-defaults (-> site-defaults
+                         ;; static 자원은 앞에서 미리 처리한다
+                         (dissoc :static)
+                         (dissoc :security) ;; TODO: AJAX CSRF 대응
+                         (assoc-in [:session :store] 세션저장소))))
 
-   (ANY "*" [] 뷰/not-found)))
+  (rfn _ 뷰/not-found))
 
 (정의 앱-dev
   (-> 앱라우트
