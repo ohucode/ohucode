63e5cdb9ff81962a544141e187e047df1bb192bf
Daehyun Kim <hatemogi@gmail.com>
2016-06-13 14:11:16 +0900

    앱상태 초기화 코드 추가

--- src-cljs/ohucode/handler.cljs
+++ src-cljs/ohucode/handler.cljs
@@ -16,7 +16,13 @@
             :페이지 {}               ; <main></main> 화면에 보여줄 페이지
             :미리보기 false          ; 개발용 미리보기 모드 on/off
             :새프로젝트 {}           ; 새프로젝트 신청/결과
+            :공간 {}                 ; 선택한 공간(네임스페이스)
+            :프로젝트 {}             ; 선택한 프로젝트
             })
+
+(register-handler
+ :초기화
+ (fn [_ _] 초기상태))
 
 (register-handler
  :가입신청
@@ -24,14 +30,14 @@
    (js/console.log "가입신청" (str 이용자))
    (POST "/signup"
        {:내용 이용자
-        :성공 (fn [내용] (dispatch [:가입결과 :성공 내용]))
-        :실패 (fn [코드 내용] (dispatch [:가입결과 :실패 내용]))})
+        :성공 (fn [내용] (dispatch [:가입신청-결과 :성공 내용]))
+        :실패 (fn [코드 내용] (dispatch [:가입신청-결과 :실패 내용]))})
    (assoc db :가입신청 {:로딩? true
                         :이용자 이용자
                         :결과 {}})))
 
 (register-handler
- :가입결과
+ :가입신청-결과
  (fn [db [_ 성패 내용]]
    (js/console.log "가입결과" (name 성패))
    (cond-> (assoc-in db [:가입신청 :로딩?] false)
@@ -43,12 +49,12 @@
    (js/console.log #js ["로그인 요청" 이용자])
    (POST "/user/login"
        {:내용 이용자
-        :성공 #(dispatch [:로그인결과 :성공 %])
-        :실패 (fn [코드 내용] (dispatch [:로그인결과 :실패 내용]))})
+        :성공 #(dispatch [:로그인요청-결과 :성공 %])
+        :실패 (fn [코드 내용] (dispatch [:로그인요청-결과 :실패 내용]))})
    (assoc db :로그인 {:로딩? true :결과 {}})))
 
 (register-handler
- :로그인결과
+ :로그인요청-결과
  (fn [db [_ 성패 내용]]
    (js/console.log "로그인결과" (name 성패) (str 내용))
    (dispatch [:페이지 :이용자홈])
@@ -66,12 +72,12 @@
    (dispatch [:페이지 :로그아웃])
    (PUT "/user/logout"
        {:내용 {}
-        :성공 #(dispatch [:로그아웃결과 :성공])
-        :실패 #(dispatch [:로그아웃결과 :실패])})
+        :성공 #(dispatch [:로그아웃-결과 :성공])
+        :실패 #(dispatch [:로그아웃-결과 :실패])})
    db))
 
 (register-handler
- :로그아웃결과
+ :로그아웃-결과
  (fn [db [_ 성패]]
    (js/console.log "로그아웃결과" (name 성패))
    (if (= :성공 성패)
@@ -96,12 +102,12 @@
      (js/console.log #js ["새프로젝트 요청" 아이디 프로젝트])
      (POST (str "/" 아이디)
          {:내용 프로젝트
-          :성공 #(dispatch [:새프로젝트결과 :성공 %])
-          :실패 (fn [코드 내용] (dispatch [:새프로젝트결과 :실패 내용]))})
+          :성공 #(dispatch [:새프로젝트-결과 :성공 %])
+          :실패 (fn [코드 내용] (dispatch [:새프로젝트-결과 :실패 내용]))})
      (assoc db :새프로젝트 {:로딩? true :결과 {}}))))
 
 (register-handler
- :새프로젝트결과
+ :새프로젝트-결과
  (fn [db [_ 성패 내용]]
    (js/console.log #js ["새프로젝트 결과" 성패 내용])
    (assoc db :새프로젝트 {:로딩? false})))
@@ -111,15 +117,30 @@
  (fn [db [_ ns]]
    (js/console.log "공간선택 register-handler" ns)
    (GET (str "/" ns)
-       {:성공 #(dispatch [:공간선택결과 :성공 %])
-        :실패 (fn [코드 내용] (dispatch [:공간선택결과 :실패 내용]))})
+       {:성공 #(dispatch [:공간선택-결과 :성공 %])
+        :실패 (fn [코드 내용] (dispatch [:공간선택-결과 :실패 내용]))})
    (assoc db :공간 {:로딩? true})))
 
 (register-handler
- :공간선택결과
+ :공간선택-결과
  (fn [db [_ 성패 내용]]
    (js/console.log (clj->js 내용))
    (assoc db :공간 (merge 내용 {:로딩? false}))))
+
+(register-handler
+ :프로젝트선택
+ (fn [db [_ ns project]]
+   (js/console.log "프로젝트선택 register-handler" ns)
+   (GET (str "/" ns "/" project)
+       {:성공 #(dispatch [:프로젝트선택-결과 :성공 %])
+        :실패 (fn [코드 내용] (dispatch [:프로젝트선택-결과 :실패 내용]))})
+   (assoc db :프로젝트 {:로딩? true})))
+
+(register-handler
+ :프로젝트선택-결과
+ (fn [db [_ 성패 내용]]
+   (js/console.log (clj->js 내용))
+   (assoc db :프로젝트 (merge 내용 {:로딩? false}))))
 
 ;;; 이하 register-sub은 subscribe를 위한 쿼리 함수를 등록한다.
 ;;; 그리고 이 함수는 뷰 컴포넌트에서 subscribe로 불러다 쓴다.
--- src-cljs/ohucode/main.cljs
+++ src-cljs/ohucode/main.cljs
@@ -4,12 +4,13 @@
             [ohucode.handler]
             [ohucode.route]
             [ohucode.top :refer [앱페이지]]
-            [re-frame.core :refer [dispatch]]
+            [re-frame.core :refer [dispatch dispatch-sync]]
             [secretary.core :as secretary]))
 
 (defn ^:export main []
   (aset js/marked.options "highlight"
         (fn [code] (.-value (.highlightAuto js/hljs code))))
+  (dispatch-sync [:초기화])
   (r/render-component [앱페이지] (.getElementById js/document "app"))
   (secretary/dispatch! js/window.location.pathname))
 
@@ -24,7 +25,7 @@
 
 (defn ^:export fig-reload []
   (js/console.log "리로드")
-  (main))
+  (js/console.debug (clj->js @re-frame.db/app-db)))
 
 (defn ^:export 로그인-알림 [이용자]
   (온로드-등록 (fn []
