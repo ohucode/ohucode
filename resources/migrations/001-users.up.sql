41890a16181debfc4f322812c34e807c7883fc99
Daehyun Kim <hatemogi@gmail.com>
2016-07-13 12:38:21 +0900

    닉네임 컬럼 추가. DDL 정돈

--- resources/migrations/001-users.up.sql
+++ resources/migrations/001-users.up.sql
@@ -1,27 +1,30 @@
 -- 이용자 기본 정보
-create table 이용자 (
-  아이디   varchar(32) primary key,
-  이메일   varchar(256) not null unique,
-  성명     varchar(64),
-  비번해쉬 varchar(32),
+CREATE TABLE 이용자 (
+  아이디   VARCHAR(32) PRIMARY KEY,
+  이메일   VARCHAR(256) NOT NULL UNIQUE,
+  닉네임   VARCHAR(64) UNIQUE,
+  성명     VARCHAR(64),
+  비번해쉬 VARCHAR(32),
 
-  코호트   int default 0,
-  요금제   int default 0,
-  소속     varchar(64),
-  거주지역 varchar(64),
-  url      varchar(256),
-  생성일시 timestamp not null default now(),
-  갱신일시 timestamp not null default now(),
-  동의일시 timestamp not null default now()
+  코호트   INT DEFAULT 0,
+  요금제   INT DEFAULT 0,
+  소속     VARCHAR(64),
+  거주지역 VARCHAR(64),
+  URL      VARCHAR(256),
+  생성일시 TIMESTAMP NOT NULL DEFAULT now(),
+  갱신일시 TIMESTAMP NOT NULL DEFAULT now(),
+  동의일시 TIMESTAMP NOT NULL DEFAULT now()
 );
 
 -- 영문아이디는 대소문자 구분하지 않고 같게 취급하지만, 보여주는 것은 구분합니다.
-create unique index 이용자_소문자_아이디_인덱스 on 이용자 (lower(아이디));
+CREATE UNIQUE INDEX 이용자_소문자_아이디_인덱스 ON 이용자 (lower(아이디));
 
-insert into 이용자 (아이디, 이메일, 성명, 비번해쉬) values
-  ('admin',    'admin@ohucode.com',   '관리자', null),
-  ('system',   'system@ohucode.com',  '시스템', null),
-  ('guest',    'guest@ohucode.com',   '손님',   null),
-  ('테스트',   'test@ohucode.com',    '오테슽', 'lqkKsfp7wFBANWfbvtcqG2QDNh8='),
-  ('미생',     'misaeng@ohucode.com', '장그래', null),
-  ('애월조단', 'dhk@ohucode.com',     '김대현', 'kkUDtH2Ee/iZ6ldzrNXpzNKHdJk=');
+INSERT INTO 이용자 (아이디, 이메일, 성명, 비번해쉬) VALUES
+  ('admin',    'admin@ohucode.com',  '관리자', NULL),
+  ('system',   'system@ohucode.com', '시스템', NULL),
+  ('guest',    'guest@ohucode.com',  '손님',   NULL),
+  ('test',     'test@ohucode.com',   '오테슽', 'vsrusjmRxBqVM+wLYA5TP5OQJVk='),
+  ('awjordan', 'dhk@ohucode.com',    '김대현', 'JGnGP3MXoUsAkRaRFHyTMOtDIqU=');
+
+-- test / test1234
+-- awjordan / jordan1234
--- resources/migrations/004-프로젝트.up.sql
+++ resources/migrations/004-프로젝트.up.sql
@@ -1,17 +1,17 @@
 -- 프로젝트 테이블
-create table 프로젝트 (
-  소유자   varchar(32) not null references 이용자(아이디) on delete cascade,
-  이름     varchar(64) not null,
-  설명     varchar(255),
-  공개     boolean not null default true,
-  생성일시 timestamp not null default now(),
-  갱신일시 timestamp not null default now(),
-  primary key (소유자, 이름)
+CREATE TABLE 프로젝트 (
+  소유자   VARCHAR(32) NOT NULL REFERENCES 이용자(아이디) ON DELETE CASCADE,
+  이름     VARCHAR(64) NOT NULL,
+  설명     VARCHAR(255),
+  공개     BOOLEAN NOT NULL DEFAULT TRUE,
+  생성일시 TIMESTAMP NOT NULL DEFAULT now(),
+  갱신일시 TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY (소유자, 이름)
 );
 
-create index 프로젝트_최신순_인덱스 on 프로젝트 (소유자, 이름, 갱신일시 desc);
+CREATE INDEX 프로젝트_최신순_인덱스 ON 프로젝트 (소유자, 이름, 갱신일시 DESC);
 
-insert into 프로젝트 (소유자, 이름, 설명, 공개) values
-  ('테스트', '빈프로젝트', '빈 프로젝트 테스트용도', true),
-  ('테스트', '테스트리포', '각종 테스트용도', true),
-  ('테스트', '비공개리포', '비공개 저장소 테스트용도', false);
+INSERT INTO 프로젝트 (소유자, 이름, 설명, 공개) VALUES
+  ('test', 'empty-repo',   '빈 프로젝트 테스트용도', TRUE),
+  ('test', 'test-repo',    '각종 테스트용도', TRUE),
+  ('test', 'private-repo', '비공개 저장소 테스트용도', FALSE);
